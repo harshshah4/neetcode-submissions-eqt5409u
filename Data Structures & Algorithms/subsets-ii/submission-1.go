7068ba6b9c189bc3b188afc40d2926e6935afb17
Harsh Shah <harshshah0110@gmail.com>
2026-04-02 09:25:23 -0700

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.go
@@ -0,0 +1,28 @@
+func subsetsWithDup(nums []int) [][]int {
+
+	sort.Ints(nums)
+	out := [][]int{}
+	var recurse func(int, []int)
+	recurse = func(i int, curr []int) {
+		if i == len(nums) {
+			ccurr := make([]int, len(curr))
+			copy(ccurr, curr)
+			out = append(out, ccurr)
+			return
+		}
+
+		curr = append(curr, nums[i])
+		fmt.Printf("curr %v\n", curr)
+		recurse(i+1, curr)
+		curr = curr[:len(curr)-1]
+		fmt.Printf("curr after pop %c\n", curr)
+
+		var j int
+		for j = i; j < len(nums) && nums[j]==nums[i]; j++ {
+			continue
+		}
+		recurse(j, curr)
+	}
+	recurse(0, []int{})
+	return out
+}
